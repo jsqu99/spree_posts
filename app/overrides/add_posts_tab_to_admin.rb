38bde1dc47301f016b989c1ca08fac399413b078
Josh Chernoff <joshc@digitalcake.us>
2013-02-27 10:48:16 -0600

    cleaned up admin views

--- app/overrides/add_posts_tab_to_admin.rb
+++ app/overrides/add_posts_tab_to_admin.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :name => "add_posts_tab_to_admin",
   :virtual_path => "spree/layouts/admin",
   :insert_bottom => "[data-hook='admin_tabs']",
-  :text => %(<%= tab(:posts) %>)
+  :text =>  "<%= tab :posts,  :url => spree.admin_posts_path, :icon => 'icon-file' %>",
 )
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,9 @@
-# class Spree::PossiblePage
-#   def self.matches?(request) 
-#     return false if request.path =~ /(^\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|t\/|tax_categories|user)+)/
-#     !Spree::Post.published.find_by_permalink(request.path[1..-1]).nil?
-#   end
-# end
-
 Spree::Core::Engine.routes.draw do
-
-  
-  
+ 
   namespace :admin do
     resources :posts
   end
-  
-  resources :posts
-  
-  # constraints(Spree::PossiblePage) do
-  #   match 'blog/(:id)', :to => 'posts#show'
-  # end
+    
+  resources :posts, :path => 'blog'
 
 end
