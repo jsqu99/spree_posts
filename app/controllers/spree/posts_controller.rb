8b962484db5747520d3e7d4a31f3cbbb766f9306
Josh Chernoff <josh@thebalefew.com>
2013-05-23 16:15:52 -0500

    removed caching

--- app/controllers/spree/posts_controller.rb
+++ app/controllers/spree/posts_controller.rb
@@ -2,7 +2,7 @@ module Spree
   class PostsController < BaseController
     before_filter { render_404 if params[:id] =~ /(\.|\\)/ }
 
-    caches_action :show, :if => Proc.new { Config[:cache_static_content] }, :layout => false
+    #caches_action :show, :if => Proc.new { Config[:cache_static_content] }, :layout => false
 
     respond_to :html
 
